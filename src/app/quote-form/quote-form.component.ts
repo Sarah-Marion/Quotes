3a4af1f41cffdc8e7724dcd789033ec0ea90a2c2
Sarah-Marion <devsarahmarion@gmail.com>
2018-04-21 09:52:41 +0300

    created input for submitted by

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuoteFormComponent implements OnInit {
 
+  quote: string;
+  author: string;
+
   constructor() { }
 
   ngOnInit() {
