b30aaaeee31a733fe5b6953db46f906fee744939
Sarah-Marion <devsarahmarion@gmail.com>
2018-04-25 16:01:50 +0300

    rounded the date count to full numbers

--- src/app/time-count.pipe.ts
+++ src/app/time-count.pipe.ts
@@ -11,7 +11,7 @@ export class TimeCountPipe implements PipeTransform {
   transform(value: number): any{
     let today:Date = new Date (); //get current date and time
     let todayWithNoTime:any = new Date(today.getFullYear(),today.getMonth(), today.getDate())
-    var dateDifference= Math.abs(value-todayWithNoTime ) //returns value in milliseconds
+    var dateDifference= todayWithNoTime-value //returns value in milliseconds
     const secondsInADay=86400; //60 sec*60 min in an hr * 24 hrs
 
     var dateDifferenceSeconds=dateDifference*0.001; //converts to seconds
@@ -19,7 +19,7 @@ export class TimeCountPipe implements PipeTransform {
     var dateCounter = dateDifferenceSeconds/secondsInADay;
 
     if (dateCounter >=1){
-      return dateCounter;
+      return Math.round(dateCounter);
     }else{
       return 0;
     }
